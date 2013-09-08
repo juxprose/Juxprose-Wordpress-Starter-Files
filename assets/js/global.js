d1585398cd36dd9904515d188ec2ca5f53d76166
David Flindall <david@juxprose.com>
2013-09-08 11:27:41 +0100

    Added Blockquote styles and basic header / paragraph formatting
    Included some JS to prepend Font Awseome icons to blockquotes within
    posts and pages. Removed some unused WP styles

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,2 +1,5 @@
 // @codekit-append "css3-mediaqueries.js"
 // @codekit-append "menu-responsive.js"
+
+// Add icon to blockquotes within posts and pages
+jQuery('.entry-content blockquote').prepend('<i class="icon-quote-left"></i>');
