130a65c15850ed2c7b8db367053c59ba34d02808
David Flindall <david@juxprose.com>
2014-03-02 10:57:45 +0000

    Fixed errors for unknown functions

--- searchform.php
+++ searchform.php
@@ -5,10 +5,10 @@
  * @package Juxprose Wordpress
  */
 ?>
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" class="search-form group" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
-		<span class="screen-reader-text"><?php _ex( 'Search for:', 'label', 'jux_wp' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'jux_wp' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php _ex( 'Search for:', 'label', 'jux_wp' ); ?>" />
+		<span class="screen-reader-text"><?php _ex( 'Search for:', 'label', 'jux_wp' ); ?></span>		
 	</label>
-	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'jux_wp' ); ?>" />
+	<input type="search" class="search-field field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'jux_wp' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php _ex( 'Search for:', 'label', 'jux_wp' ); ?>" />
+	<input type="submit" class="search-submit btn" value="<?php echo esc_attr_x( 'Search', 'submit button', 'jux_wp' ); ?>" />
 </form>
--- sidebar.php
+++ sidebar.php
@@ -5,14 +5,14 @@
  * @package Juxprose Wordpress
  */
 ?>
-	<div id="secondary" class="widget-area" role="complementary">
+	<div id="secondary" class="widget-area group" role="complementary">
 		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
-			<aside id="search" class="widget widget_search">
+			<aside id="search" class="widget widget_search group">
 				<?php get_search_form(); ?>
 			</aside>
 
-			<aside id="archives" class="widget">
+			<aside id="archives" class="widget group">
 				<h1 class="widget-title"><?php _e( 'Archives', 'jux_wp' ); ?></h1>
 				<ul>
 					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
