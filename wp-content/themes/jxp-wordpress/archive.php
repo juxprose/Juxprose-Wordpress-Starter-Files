10ef5abfc0b16f113f8130c51fd2a83b32212b75
David Flindall <david@juxprose.com>
2015-10-16 09:19:42 +0100

    Updated archive page

--- wp-content/themes/jxp-wordpress/archive.php
+++ wp-content/themes/jxp-wordpress/archive.php
@@ -9,28 +9,41 @@
 
 get_header(); ?>
 
-<?php if ( have_posts() ) : ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-	<header class="page-header">
-		<?php
-			the_archive_title( '<h1 class="page-title">', '</h1>' );
-			the_archive_description( '<div class="taxonomy-description">', '</div>' );
-		?>
-	</header><!-- .page-header -->
+		<?php if ( have_posts() ) : ?>
 
-	<?php while ( have_posts() ) : the_post(); ?>
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
 
-		<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-	<?php endwhile; ?>
+				<?php
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'partials/content', get_post_format() );
+				?>
 
-	<?php the_posts_navigation(); ?>
+			<?php endwhile; ?>
 
-<?php else : ?>
+			<?php the_posts_navigation(); ?>
 
-	<?php get_template_part( 'content', 'none' ); ?>
+		<?php else : ?>
 
-<?php endif; ?>
+			<?php get_template_part( 'partials/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
